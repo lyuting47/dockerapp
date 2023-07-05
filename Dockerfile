0cc9b440b69c36586f75e31055d0867dea031ec6
lyuting47 <90270519+lyuting47@users.noreply.github.com>
2023-07-05 09:06:07 +0800

    Add files via upload

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,7 @@ COPY . .
 RUN ["npm", "run", "build"]
 
 FROM nginx:1.24
-ENV NODE_ENV=production
+COPY nginx.conf /etc/nginx/conf.d/default.conf
 COPY --from=builder /app/dist /usr/share/nginx/html/
-EXPOSE 80
+EXPOSE 3000
 CMD ["nginx", "-g", "daemon off;"]
--- vite.config.ts
+++ vite.config.ts
@@ -14,10 +14,10 @@ export default defineConfig({
   test: {
     globals: true,
     environment: "jsdom",
-    setupFiles: "./src/setupTests.ts",
+    setupFiles: "./src/MsalReactTester/setupTests.ts",
     coverage: {
       reporter: ["text", "html"],
-      exclude: ["node_modules/", "src/setupTests.ts", "src/MsalReactTester/"],
+      exclude: ["node_modules/", "src/MsalReactTester/"],
     },
   },
 });
