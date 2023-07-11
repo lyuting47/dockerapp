a3f2979c2bc46b298d74acd3735a76a4a7ad9bef
lyuting47 <90270519+lyuting47@users.noreply.github.com>
2023-07-11 11:27:55 +0800

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -20,4 +20,19 @@ export default defineConfig({
       exclude: ["node_modules/", "src/MsalReactTester/"],
     },
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        },
+      },
+    },
+  },
 });
